bbd87d3330509bcb66d9a4f613fb5d60e51c9c93
Dave Edwards <daveedwards@Daves-MacBook-Pro-13-Inch.local>
2021-02-20 13:53:47 +0000

    added faker, env & function for creating x users

--- app.py
+++ app.py
@@ -1,14 +1,28 @@
 import pugsql
+from faker import Faker
+
 from dotenv import load_dotenv
+import sys
 import os
+
+# load the .env file
 load_dotenv()
+
+fake = Faker()
 
 DB = os.getenv("DATABASE")
 queries = pugsql.module('queries/')
 queries.connect(DB)
 
-queries.create_user({
-    'first_name': "Dave",
-    "last_name": "Edwards",
-    "email": "Dave@test2.com",
-    "phone": "01234567"})
+
+def create_users(qty):
+    for i in range(qty):
+        queries.create_user({
+            'first_name': fake.first_name(),
+            "last_name": fake.last_name(),
+            "email": fake.email(),
+            "phone": fake.phone_number()})
+
+
+if __name__ == "__main__":
+    create_users(int(sys.argv[1]))
